3cd79435c6d20b4f182c61ff0fbf68f9a99a29cf
sadness_ojisan <sadness.ojisan@gmail.com>
2020-02-23 14:52:40 +0900

    update: production mode + devtool: "inline-source-map"

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "main.js"
-  }
+  },
+  devtool: "inline-source-map"
 };
